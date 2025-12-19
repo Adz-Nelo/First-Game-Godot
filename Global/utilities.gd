e671cb0c641240299ff7d47dbd609df9d44da507
Adz-Nelo <adz.nelo155@gmail.com>
2025-12-19 13:44:50 +0800

    added collectable items

--- Global/utilities.gd
+++ Global/utilities.gd
@@ -1,20 +1,19 @@
 extends Node
 
-const save_path = "user://savegame.bin"
+const save_path = "res://savegame.bin"
 
 func saveGame():
 	var file = FileAccess.open(save_path, FileAccess.WRITE)
 	var data: Dictionary = {
 		"playerHP": Game.playerHP,
-		"gold": Game.gold, 
+		"gold": Game.gold,   
 	}
 	
 	var json_str = JSON.stringify(data)
 	file.store_line(json_str)
 	
 func loadGame():
-	var file = FileAccess.open(save_path, FileAccess.READ)
-	
+	var file = FileAccess.open(save_path, FileAccess.READ) 
 	if FileAccess.file_exists(save_path) == true:
 		if not file.eof_reached():
 			var current_line = JSON.parse_string(file.get_line())
