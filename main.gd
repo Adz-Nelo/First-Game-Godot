22954333bd67faa14147c2644c7e33d37fcaaf8d
Adz-Nelo <adz.nelo155@gmail.com>
2026-01-17 23:34:10 +0800

    w/ sound-effects & pause menu + child node functional but w/ mere bugs

--- main.gd
+++ main.gd
@@ -8,10 +8,13 @@ func _ready() -> void:
 	Utilities.loadGame()
 
 func _on_quit_pressed() -> void:
+	AudioController.select_sound()
 	get_tree().quit()
 
 func _on_play_pressed() -> void:
+	AudioController.select_sound()
 	get_tree().change_scene_to_file("res://world.tscn")
 
 func _on_option_pressed() -> void:
+	AudioController.select_sound()
 	get_tree().change_scene_to_file("res://options.tscn")
